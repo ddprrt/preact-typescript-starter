95f9cb1ad85acd19d6ba202a9fb52d7a2b5f8af2
Baumgartner <Stefan.Baumgartner@dynatrace.com>
2020-10-13 09:36:57 +0200

    initial

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { h, render } from "preact";
+
+function App() {
+  return <h1>Hello World</h1>;
+}
+
+const node = document.querySelector("#app");
+if (node) {
+  render(<App />, node);
+}
+
+export default {};
