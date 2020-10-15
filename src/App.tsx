2b4db792733858d2dcbe6e951e7dce052dd2c9bb
Baumgartner <Stefan.Baumgartner@dynatrace.com>
2020-10-15 15:26:57 +0200

    try react-jsx w Preact

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import { h, render } from "preact";
+import { render } from "preact";
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1>Hello World</h1>
+    </>
+  );
 }
+
+const el = jsx("div", { class: "Hello" });
 
 const node = document.querySelector("#app");
 if (node) {
